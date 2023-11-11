868205f76ff8d10417c8168693e200727ef4f438
Diego Cáceres Cardoza <dcaceres@mym.com.pe>
2023-11-11 08:51:22 -0500

    init

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const baseAPI = axios.create({
+  baseURL: '//rimac-front-end-challenge.netlify.app/api/'
+})
+
+export const getUser = async () => {
+  const res = await baseAPI.get(`/user.json`)
+  return res.data
+}
+
+export const getPlan = async () => {
+  const res = await baseAPI.get(`/plan.json`)
+  return res.data
+}
